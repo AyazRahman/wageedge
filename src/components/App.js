13df96e1582a9d4672084563513f50742a12b419
muhtasim3504 <muhtasim_ayaz@yahoo.com>
2019-08-19 01:40:39 +1000

    Added about page

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 
 import history from "../history";
-import Navigation from "./Navigation";
-import Home from "./Home";
-import Footer from "./Footer";
-import Laws from "./Laws";
+import Navigation from "./common/Navigation";
+import Home from "./pages/Home";
+import Footer from "./common/Footer";
+import Laws from "./pages/Laws";
+import About from "./pages/About";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/laws" exact component={Laws} />
+            <Route path="/about" exact component={About} />
           </Switch>
         </div>
         <Footer />
---
+++ src/components/pages/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Header from "../Header";
+
+const About = () => {
+  return (
+    <React.Fragment>
+      <Header image="img/header-bg.jpg" />
+      <section className="page-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <h2 className="section-heading text-uppercase">
+                Team: Infinite5
+              </h2>
+              <h3 className="section-subheading text-muted">
+                We are trying to solve a Victorian Economic Issue
+              </h3>
+            </div>
+          </div>
+          <div className="row ">TBD</div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default About;
